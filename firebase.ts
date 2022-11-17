a5842933d20423b86e235e7b7b5c8854c6e33551
Mykola Zahanych <mykola.zahanych@accenture.com>
2022-11-17 17:23:13 +0100

    day 2

---
+++ firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_DATA",
+  authDomain: "YOUR_DATA",
+  projectId: "YOUR_DATA",
+  storageBucket: "YOUR_DATA",
+  messagingSenderId: "YOUR_DATA",
+  appId: "YOUR_DATA",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { auth, db };
