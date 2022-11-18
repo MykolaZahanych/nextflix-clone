f44140f30ddbfdd0284332a6e3d8633e5e4c44c3
Mykola Zahanych <mykola.zahanych@accenture.com>
2022-11-18 10:56:54 +0100

    small fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head';
-import { useRecoilValue } from 'recoil';
-import { modalState } from '../atoms/modalAtom';
-import Banner from '../components/Banner';
-import Header from '../components/Header';
-import Modal from '../components/Modal';
-import Row from '../components/Row';
-import useAuth from '../hooks/useAuth';
-import { Movie } from '../typings';
-import requests from '../utils/request';
+import Head from "next/head";
+import { useRecoilValue } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
+import { Movie } from "../typings";
+import requests from "../utils/request";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -31,31 +31,39 @@ const Home = ({
   trendingNow,
 }: Props) => {
   const { loading } = useAuth();
-  const showModal = useRecoilValue(modalState)
+  const showModal = useRecoilValue(modalState);
+  const subscription = false
 
-  if (loading) return null
+  if (loading || subscription === null) return null;
+  
+  if (!subscription) return <div>Plans</div>
+
   return (
-    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
         <title>Home - Nextflix</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section className='md:space-y-24'>
-          <Row title='Trending Now' movies={trendingNow} />
-          <Row title='Top Rated' movies={topRated} />
-          <Row title='Action Thrillers' movies={actionMovies} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
           {/* My list component here */}
-          <Row title='Comedies' movies={comedyMovies} />
-          <Row title='Scary Movies' movies={horrorMovies} />
-          <Row title='Romance Movies' movies={romanceMovies} />
-          <Row title='Documentaries' movies={documentaries} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      { showModal && <Modal /> }
+      {showModal && <Modal />}
     </div>
   );
 };
